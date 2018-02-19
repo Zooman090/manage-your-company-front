8d8cd1966fddea26155df289f64b55423a97be5d
Dmytro Paniontko <zooman090@gmail.com>
2018-02-19 13:57:27 +0200

    Created root components.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, Route } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { createHashHistory } from 'history';
 
 import './styles/app.scss';
@@ -11,14 +11,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 /* eslint-enable */
 
-import Header from './js/components/header';
-import Main from './js/components/main-page/index.jsx';
-import CreateCompany from './js/components/company/create';
-import CreateStaff from './js/components/staff/create.jsx';
-import SignUp from './js/components/sign/up.jsx';
-import SignIn from './js/components/sign/in.jsx';
-
-import Dialog from './js/components/dialog';
+import Root from './js/components/root';
 
 import reducer from './js/reducers/index.js';
 import localStorageLoad from './js/middleware/local-storage-load';
@@ -31,15 +24,7 @@ store.dispatch({ type: 'INIT' });
 render(
   <Provider store={store}>
     <Router history={history}>
-      <div>
-        <Dialog />
-        <Header />
-        <Route exact path="manage-your-company-front/" component={Main} />
-        <Route path="/company-create" component={CreateCompany} />
-        <Route path="/staff-create" component={CreateStaff} />
-        <Route path="/sign-up" component={SignUp} />
-        <Route path="/sign-in" component={SignIn} />
-      </div>
+      <Root />
     </Router>
   </Provider>,
   document.getElementById('app')
