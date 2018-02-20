a60a5bcd7d79fac95710732f4230bc7b12011d80
Dmytro Paniontko <zooman090@gmail.com>
2018-02-20 10:43:23 +0200

    Cganhed server url

--- src/constants/route.js
+++ src/constants/route.js
@@ -1,5 +1,5 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
-export const serverUrl = isProduction ? 'https://fathomless-basin-64420.herokuapp.com' : 'http://localhost:8008';
+export const serverUrl = isProduction ? 'https://manage-your-companies.herokuapp.com' : 'http://localhost:8008';
 
 export const siteUrl = isProduction ? 'https://zooman090.github.io' : 'http://localhost:8080';
